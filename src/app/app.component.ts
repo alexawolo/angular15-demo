98fffd6972e478bc444abd7c48f6b8bd16dd8fac
Alexa Woloszuk <alexawoloszuk@gmail.com>
2022-12-07 16:35:17 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular15';
+}
